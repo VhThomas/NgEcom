0ae185c8435bee1aa2015ddcbe95347cb51757aa
Thomas <thomasvanherpe79@gmail.com>
2018-03-24 00:07:51 +0100

    initial setup + install of fixed firebase packages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDSmNCNad0MrcOAJtXQ0j5Hlt_psIzXuiA",
+    authDomain: "ngecom-webismore.firebaseapp.com",
+    databaseURL: "https://ngecom-webismore.firebaseio.com",
+    projectId: "ngecom-webismore",
+    storageBucket: "ngecom-webismore.appspot.com",
+    messagingSenderId: "673864478515"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDSmNCNad0MrcOAJtXQ0j5Hlt_psIzXuiA",
+    authDomain: "ngecom-webismore.firebaseapp.com",
+    databaseURL: "https://ngecom-webismore.firebaseio.com",
+    projectId: "ngecom-webismore",
+    storageBucket: "ngecom-webismore.appspot.com",
+    messagingSenderId: "673864478515"
+  }
 };
